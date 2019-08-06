884f6e304faefc2dfff709ef77e2726334b84809
Nick Huanca <nick@fairwinds.com>
2019-08-06 12:39:49 -0600

    initial refactor testing webhook init

--- test/webhook_test.sh
+++ test/webhook_test.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
+
 #Testing to ensure that the webhook starts up, allows a correct deployment to pass,
 #and prevents a incorrectly formatted deployment. 
 set -e
 #sed is replacing the polaris version with this commit sha so we are testing exactly this verison.
 sed -ri "s|'(quay.io/reactiveops/polaris:).+'|'\1${CIRCLE_SHA1}'|" ./deploy/webhook.yaml
-kubectl apply -f ./deploy/webhook.yaml &> /dev/null
-timeout=25
-#Fix Me: Need a more deterministic way to test for completion of webhook installation.
-#The while loop exits when the webhook is installed but not yet effective, so we have to sleep
-#before testing it.
-while ! kubectl get pods -n polaris | grep "polaris-webhook.*Running"; do
-  echo "Waiting for webhook to start..."
-  if [ $timeout -eq 0 ]; then
-    echo "Timed out while waiting for webhook to start"
-    exit 1
-  fi
-  timeout=$((timeout-1))
-  sleep 1
-done
-sleep 30
-echo "Webhook started!"
 
-#Webhook started, setting all tests as passed initially.
+function check_webhook_is_ready() {
+  echo "Waiting for webhook to be ready"
+  # Get the epoch time in one minute from now
+  local timeout_epoch=$(date -d "+1 minute" +%s)
+  # loop until this fails (desired condition is we cannot apply this yaml doc, which means the webhook is working
+  while kubectl apply -f failing_test.deployment.yaml; do
+    if [[ "$(date +%s)" -ge "${timeout_epoch}" ]]; then
+      echo -e "Timeout hit waiting for webhook readiness: exiting"
+      exit 1
+    fi
+    echo -n "."
+    kubectl -n polaris get po -oyaml
+    kubectl -n polaris logs --tail 10 -l app=polaris
+    sleep 0.5
+  done
+  # clean up the test (the "or true" is to catch the condition that it couldn't delete something
+  kubectl delete -f failing_test.deployment.yaml || true
+  echo "Webhook started!"
+}
+
+# Install the webhook
+kubectl apply -f ./deploy/webhook.yaml &> /dev/null
+
+# wait for the webhook to come online
+check_webhook_is_ready
+
+# Run the tests
+# Webhook started, setting all tests as passed initially.
 ALL_TESTS_PASSED=1
 
 for filename in test/passing_test.*.yaml; do
